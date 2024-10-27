cd4445f6c89f9bec9fa4ff7e7b6686688259eab9
sarvagyapatel <103870100+sarvagyapatel@users.noreply.github.com>
2024-10-27 22:49:54 +0530

    Update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
+module.exports = {
+    typescript: {
+      ignoreBuildErrors: true, // Ignore TypeScript errors during build
+    },
+  };
+  
 export default nextConfig;
